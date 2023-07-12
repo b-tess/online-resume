845604d5ee319d22b5eac1079e6ee54e11bbf8ae
LogSyn <barbaratessn@gmail.com>
2023-07-12 23:49:39 +0300

    Feature styling display issue

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const leftCover = document.querySelector('.cover')
+const leftCover = document.querySelector('.left-cover')
+const rightCover = document.querySelector('.right-cover')
+const sliderContainer = document.querySelector('.slider-feature-container')
 const button = document.querySelector('button')
 
 button.addEventListener('click', () => {
     leftCover.classList.add('uncover')
+    rightCover.classList.add('uncover')
+    sliderContainer.classList.add('uncover')
 })
